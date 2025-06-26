1e45ce8ad81579337b6d6762494f3d1c7eeddacf
Patrick Höveler <patrick.hoeveler@de.ey.com>
2025-06-26 08:29:42 +0200

    refine query prompt to exclude system tables

--- text-to-sql-team-red/skill/qa.py
+++ text-to-sql-team-red/skill/qa.py
@@ -218,6 +218,11 @@ if __name__ == "__main__":
     from pharia_skill.testing import DevCsi
 
     csi = DevCsi()
-    res = custom_rag(csi, Input(question="Please query the number of tables."))
+    res = custom_rag(
+        csi,
+        Input(
+            question="Please query the number of tables - exclude the system tables."
+        ),
+    )
     print("---- result ----")
     print(res.answer)
